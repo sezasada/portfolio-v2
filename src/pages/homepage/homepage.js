ade3b5afa49b7d93a49137b7302aa34bed98ec6b
sezasada <sezasada@icloud.com>
2024-08-27 16:45:18 -0500

    created navbar component

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Navbar from "../../components/Navbar/Navbar";
+import css from "./homepage.module.css";
 
 const Homepage = () => {
   return (
-    <div>
+    <div className={css.pageWrapper}>
+      <Navbar />
       <Header />
     </div>
   );
