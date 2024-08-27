358b056c450bf16d11379d2f6c938b2eb694f608
sezasada <sezasada@icloud.com>
2024-08-27 10:14:14 -0500

    updated title, subtitle, and contact button styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,27 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
-import { PrimaryButton } from "../../design/Button/Button";
+import { SecondaryButton } from "../../design/Button/Button";
 import css from "./Header.module.css";
 
 const Header = () => {
   return (
-    <>
-      <div className={css.titleStyles}>
-        <FormattedMessage id="Header.title" />
+    <div className={css.wrapperStyles}>
+      <div className={css.contentStyles}>
+        <div className={css.titleStyles}>
+          <FormattedMessage id="Header.title" />
+        </div>
+        <div className={css.subTitleStyles}>
+          <FormattedMessage id="Header.subTitle" />
+        </div>
+        <div>
+          <SecondaryButton className={css.contactButton} type="button">
+            <div>
+              <FormattedMessage id="Header.contactButton" />
+            </div>
+          </SecondaryButton>
+        </div>
       </div>
-      <div className={css.subTitleStyles}>
-        <FormattedMessage id="Header.subTitle" />
-      </div>
-      <div>
-        <PrimaryButton className={css.contactButton} type="button">
-          <div>
-            <FormattedMessage id="Header.contactButton" />
-          </div>
-        </PrimaryButton>
-      </div>
-    </>
+    </div>
   );
 };
 
