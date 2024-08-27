c83393b4a3597aef2420cf97fab51c2e8b19ad89
sezasada <sezasada@icloud.com>
2024-08-27 09:10:59 -0500

    setup folders. installed react-intl

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+const Header = () => {
+  return (
+    <div>
+      <FormattedMessage id="Header.title" />
+    </div>
+  );
+};
+
+export default Header;
