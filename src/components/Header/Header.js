3924f4b81d668cb74a240f252e62f1f3f75b4c86
sezasada <sezasada@icloud.com>
2024-10-25 20:44:59 -0500

    added some functions to ContactMe component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,9 @@ import profileImage from "@public/images/header-images/profile-image.png";
 import css from "./Header.module.css";
 
 const Header = () => {
+  const handleContactClick = () => {
+    window.location.href = "mailto:sezasada@icloud.com";
+  };
   return (
     <div className={css.wrapperStyles}>
       <div className={css.contentStyles}>
@@ -15,7 +18,11 @@ const Header = () => {
           <FormattedMessage id="Header.subTitle" />
         </div>
         <div>
-          <SecondaryButton className={css.contactButton} type="button">
+          <SecondaryButton
+            className={css.contactButton}
+            type="button"
+            onClick={handleContactClick}
+          >
             <div>
               <FormattedMessage id="Header.contactButton" />
             </div>
