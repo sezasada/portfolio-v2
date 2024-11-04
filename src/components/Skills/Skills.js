940002d8ecd75bcef99ff97700e38316ae14692f
sezasada <sezasada@icloud.com>
2024-11-04 16:22:11 -0600

    Sezasada/new testimonials (#18)
    * added nics testimonial. updated design or testimonial cards
    
    * lots of design updates. imported Marquee for testimonials and skills section
    
    * removed robs testimonial for now, waiting on the rest of it

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React from "react";
 import { FormattedMessage } from "react-intl";
 import { ReactComponent as HtmlIcon } from "../../assets/icons/html.svg";
 import { ReactComponent as GraphqlIcon } from "../../assets/icons/graphql.svg";
@@ -9,129 +9,55 @@ import { ReactComponent as AwsIcon } from "../../assets/icons/aws.svg";
 import { ReactComponent as ReduxIcon } from "../../assets/icons/redux.svg";
 import { ReactComponent as ReactIcon } from "../../assets/icons/react.svg";
 import { ReactComponent as NextIcon } from "../../assets/icons/next.svg";
+import Marquee from "react-fast-marquee";
 
 import css from "./Skills.module.css";
 
 const Skills = () => {
-  const skills = [
-    { id: "Skills.javascript", Icon: JavascriptIcon },
-    { id: "Skills.typescript", Icon: TypescriptIcon },
-    { id: "Skills.graphql", Icon: GraphqlIcon },
-    { id: "Skills.html", Icon: HtmlIcon },
-    { id: "Skills.css", Icon: CssIcon },
-    { id: "Skills.aws", Icon: AwsIcon },
-    { id: "Skills.redux", Icon: ReduxIcon },
-    { id: "Skills.react", Icon: ReactIcon },
-    { id: "Skills.next", Icon: NextIcon },
-  ];
-
-  const skillsToShow = 5;
-  const [currentSkillIndex, setCurrentSkillIndex] = useState(0);
-
-  const displayedSkills = skills.slice(
-    currentSkillIndex,
-    currentSkillIndex + skillsToShow
-  );
-
-  // Function to update the index
-  const updateIndex = (newIndex) => {
-    if (newIndex < 0) {
-      setCurrentSkillIndex(skills.length - skillsToShow);
-    } else if (newIndex > skills.length - skillsToShow) {
-      setCurrentSkillIndex(0);
-    } else {
-      setCurrentSkillIndex(newIndex);
-    }
-  };
-
-  // Function to go to the previous set of skills
-  const prev = useCallback(
-    (e) => {
-      if (e) {
-        e.preventDefault();
-        e.stopPropagation();
-      }
-      const newIndex = currentSkillIndex - 1;
-      updateIndex(newIndex);
-    },
-    [currentSkillIndex, skills.length, skillsToShow]
-  );
-
-  // Function to go to the next set of skills
-  const next = useCallback(
-    (e) => {
-      if (e) {
-        e.preventDefault();
-        e.stopPropagation();
-      }
-      const newIndex = currentSkillIndex + 1;
-      updateIndex(newIndex);
-    },
-    [currentSkillIndex, skills.length, skillsToShow]
-  );
-
-  // Keyboard navigation
-  const onKeyUp = useCallback(
-    (e) => {
-      switch (e.keyCode) {
-        case 37: // Left arrow key
-          prev();
-          break;
-        case 39: // Right arrow key
-          next();
-          break;
-        default:
-      }
-    },
-    [prev, next]
-  );
-
-  useEffect(() => {
-    window.addEventListener("keyup", onKeyUp);
-
-    return () => {
-      window.removeEventListener("keyup", onKeyUp);
-    };
-  }, [onKeyUp]);
-
   return (
     <div className={css.wrapperStyles}>
-      <div className={css.titleStyles}>
-        <FormattedMessage id="Skills.title" />
-      </div>
-      <div className={css.carouselWrapper}>
-        <div className={css.controlsContainer}>
-          {skills.length > 1 && (
-            <>
-              <button
-                className={css.prev}
-                onClick={(e) => prev(e)}
-                type="button"
-                aria-label="Previous skill"
-              />
-              <div className={css.skillsContainer}>
-                <div className={css.skillsWrapper}>
-                  {displayedSkills.map(({ id, Icon }, index) => (
-                    <div key={index} className={css.skillWrapper}>
-                      {Icon && (
-                        <div>
-                          <Icon className={css.iconStyles} />
-                        </div>
-                      )}
-                      <FormattedMessage id={id} />
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <button
-                className={css.next}
-                onClick={(e) => next(e)}
-                type="button"
-                aria-label="Next skill"
-              />
-            </>
-          )}
+      <div className={css.containerStyles}>
+        <div className={css.titleStyles}>
+          <FormattedMessage id="Skills.title" />
         </div>
+        <Marquee autoFill>
+          <div className={css.skillWrapper}>
+            <JavascriptIcon className={css.iconStyles} />
+            <FormattedMessage id="Skills.javascript" />
+          </div>
+          <div className={css.skillWrapper}>
+            <TypescriptIcon className={css.iconStyles} />
+            <FormattedMessage id="Skills.typescript" />
+          </div>
+          <div className={css.skillWrapper}>
+            <GraphqlIcon className={css.iconStyles} />
+            <FormattedMessage id="Skills.graphql" />
+          </div>
+          <div className={css.skillWrapper}>
+            <HtmlIcon className={css.iconStyles} />
+            <FormattedMessage id="Skills.html" />
+          </div>
+          <div className={css.skillWrapper}>
+            <CssIcon className={css.iconStyles} />
+            <FormattedMessage id="Skills.css" />
+          </div>
+          <div className={css.skillWrapper}>
+            <AwsIcon className={css.iconStyles} />
+            <FormattedMessage id="Skills.aws" />
+          </div>
+          <div className={css.skillWrapper}>
+            <ReduxIcon className={css.iconStyles} />
+            <FormattedMessage id="Skills.redux" />
+          </div>
+          <div className={css.skillWrapper}>
+            <ReactIcon className={css.iconStyles} />
+            <FormattedMessage id="Skills.react" />
+          </div>
+          <div className={css.skillWrapper}>
+            <NextIcon className={css.iconStyles} />
+            <FormattedMessage id="Skills.next" />
+          </div>
+        </Marquee>
       </div>
     </div>
   );
--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -2,11 +2,30 @@ import React from "react";
 import { FormattedMessage } from "react-intl";
 import codyImage from "../../assets/images/testimonials/cody.jpg";
 import joshImage from "../../assets/images/testimonials/josh.jpg";
+import nicImage from "../../assets/images/testimonials/nic.jpg";
+// import robImage from "../../assets/images/testimonials/rob.webp";
 import { ReactComponent as Quotes } from "../../assets/icons/quotes.svg";
+import Marquee from "react-fast-marquee";
 
 import css from "./Testimonials.module.css";
 
 const Testimonials = () => {
+  const handleCodyClick = () => {
+    window.open("https://www.linkedin.com/in/cody-allen-hebert/", "_blank");
+  };
+
+  const handleJoshClick = () => {
+    window.open("https://www.linkedin.com/in/jhint/", "_blank");
+  };
+
+  // const handleRobClick = () => {
+  //   window.open("https://www.linkedin.com/in/rob-rotarius-97b49812/", "_blank");
+  // };
+
+  const handleNicClick = () => {
+    window.open("https://www.linkedin.com/in/nicdecastro/", "_blank");
+  };
+
   return (
     <div className={css.containerStyles}>
       <div className={css.wrapperStyles}>
@@ -18,54 +37,102 @@ const Testimonials = () => {
             <FormattedMessage id="Testimonials.subtitle" />
           </div>
           <hr className={css.divider} />
-          <div className={css.testimonialContainer}>
-            <div className={css.testimonialWrapper}>
-              <div className={css.testimonialBox}>
-                <img
-                  src={codyImage}
-                  alt="employee"
-                  className={css.sectionImage}
-                />
-                <div className={css.testimonialBio}>
-                  <div className={css.testimonialName}>
-                    <FormattedMessage id="Testimonials.cody" />
+          <Marquee autoFill pauseOnHover>
+            <div className={css.testimonialContainer}>
+              <div className={css.testimonialWrapper}>
+                <div className={css.testimonialBox} onClick={handleCodyClick}>
+                  <img
+                    src={codyImage}
+                    alt="employee"
+                    className={css.sectionImage}
+                  />
+                  <div className={css.testimonialBio}>
+                    <div className={css.testimonialName}>
+                      <FormattedMessage id="Testimonials.cody" />
+                    </div>
+                    <div className={css.testimonialJonTitle}>
+                      <FormattedMessage id="Testimonials.codyJobTitle" />
+                    </div>
                   </div>
-                  <div className={css.testimonialJonTitle}>
-                    <FormattedMessage id="Testimonials.codyJobTitle" />
+                  <div className={css.quoteIconStyles}>
+                    <Quotes />
                   </div>
                 </div>
-                <div className={css.quoteIconStyles}>
-                  <Quotes />
+                <div className={css.testimonialReferral}>
+                  <FormattedMessage id="Testimonials.codyReferral" />
                 </div>
               </div>
-              <div className={css.testimonialReferral}>
-                <FormattedMessage id="Testimonials.codyReferral" />
+              <div className={css.testimonialWrapper}>
+                <div className={css.testimonialBox} onClick={handleJoshClick}>
+                  <img
+                    src={joshImage}
+                    alt="employee"
+                    className={css.sectionImage}
+                  />
+                  <div className={css.testimonialBio}>
+                    <div className={css.testimonialName}>
+                      <FormattedMessage id="Testimonials.josh" />
+                    </div>
+                    <div className={css.testimonialJonTitle}>
+                      <FormattedMessage id="Testimonials.joshJobTitle" />
+                    </div>
+                  </div>
+                  <div className={css.quoteIconStyles}>
+                    <Quotes />
+                  </div>
+                </div>
+                <div className={css.testimonialReferral}>
+                  <FormattedMessage id="Testimonials.joshReferral" />
+                </div>
               </div>
+              <div className={css.testimonialWrapper}>
+                <div className={css.testimonialBox} onClick={handleNicClick}>
+                  <img
+                    src={nicImage}
+                    alt="employee"
+                    className={css.sectionImage}
+                  />
+                  <div className={css.testimonialBio}>
+                    <div className={css.testimonialName}>
+                      <FormattedMessage id="Testimonials.nic" />
+                    </div>
+                    <div className={css.testimonialJonTitle}>
+                      <FormattedMessage id="Testimonials.nicJobTitle" />
+                    </div>
+                  </div>
+                  <div className={css.quoteIconStyles}>
+                    <Quotes />
+                  </div>
+                </div>
+                <div className={css.testimonialReferral}>
+                  <FormattedMessage id="Testimonials.nicReferral" />
+                </div>
+              </div>
+              {/* <div className={css.testimonialWrapper}>
+                <div className={css.testimonialBox} onClick={handleRobClick}>
+                  <img
+                    src={robImage}
+                    alt="employee"
+                    className={css.sectionImage}
+                  />
+                  <div className={css.testimonialBio}>
+                    <div className={css.testimonialName}>
+                      <FormattedMessage id="Testimonials.rob" />
+                    </div>
+                    <div className={css.testimonialJonTitle}>
+                      <FormattedMessage id="Testimonials.robJobTitle" />
+                    </div>
+                  </div>
+                  <div className={css.quoteIconStyles}>
+                    <Quotes />
+                  </div>
+                </div>
+                <div className={css.testimonialReferral}>
+                  <FormattedMessage id="Testimonials.robReferral" />
+                </div>
+              </div> */}
             </div>
-            <div className={css.testimonialWrapper}>
-              <div className={css.testimonialBox}>
-                <img
-                  src={joshImage}
-                  alt="employee"
-                  className={css.sectionImage}
-                />
-                <div className={css.testimonialBio}>
-                  <div className={css.testimonialName}>
-                    <FormattedMessage id="Testimonials.josh" />
-                  </div>
-                  <div className={css.testimonialJonTitle}>
-                    <FormattedMessage id="Testimonials.joshJobTitle" />
-                  </div>
-                </div>
-                <div className={css.quoteIconStyles}>
-                  <Quotes />
-                </div>
-              </div>
-              <div className={css.testimonialReferral}>
-                <FormattedMessage id="Testimonials.joshReferral" />
-              </div>
-            </div>
-          </div>
+          </Marquee>
         </div>
       </div>
     </div>
