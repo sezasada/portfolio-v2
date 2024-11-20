e4a6dd28de879bf5d5604bb1c28cf7bac0c3f454
sezasada <sezasada@icloud.com>
2024-11-20 16:07:29 -0600

    installed vercel packages (#29)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Homepage from "./pages/homepage/homepage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { IntlProvider } from "react-intl";
 import enMessages from "./translations/en.json";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const locale = "en";
@@ -9,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <IntlProvider locale={locale} messages={enMessages}>
+        <Analytics />
         <Routes>
           <Route path="/" element={<Homepage />} />
         </Routes>
